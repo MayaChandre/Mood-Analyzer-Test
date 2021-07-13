6346ea532fad19e6793a3faf1a21889292c98db0
mayavathi <mayachandre27@gmail.com>
2021-07-13 08:22:25 -0400

    Exception Handling

--- src/test/java/Mood/Analyzer/Test/MoodAnalyzer.java
+++ src/test/java/Mood/Analyzer/Test/MoodAnalyzer.java
@@ -2,18 +2,26 @@ package Mood.Analyzer.Test;
 
 public class MoodAnalyzer {
 	 private String message;
-	    public String analyseMood() {
-	        if (message.contains("Sad"))
-	            return "SAD";
-	        else
-	            return "HAPPY";
-	    }
 
 	    
 	    public MoodAnalyzer(String message) {
 	        this.message = message;
 	    }
 
+	 // Check Sad or Happy Mood
+		public String analyseMood() {
+			try {
+				if (message.toLowerCase().contains("sad"))
+					return "SAD";
+
+				return "HAPPY";
+			} 
+			catch (NullPointerException e) {
+				throw new NullPointerException("Invalid Name");
+
+			}
+
+		}
 	}
 	
 	
